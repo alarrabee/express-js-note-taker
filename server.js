17022ae1d95d9bf5073cd766ad5013dcd7527120
alarrabee <alexlarrabee10@gmail.com>
2024-05-13 10:42:14 -0500

    set up basic server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express'); //imports the express module
+const app = express(); //sets up an instance of the express method
+
+//sets up the PORT that will listen for incoming connections on the port specified by the PORT environment variable, falling back to port 3001 if the environment variable is not set.
+const PORT = process.env.PORT || 3001;
+
+//middleware for serving static files to the public without needing to define routes for each individual file
+app.use(express.static('public'));
+
+//middleware for posting to APIs
+app.use(express.json()); //will parse incoming JSON requests and make that data available in the req.body
+app.use(express.urlencoded({extended:true})); //will parse incoming URL-encoded requests and make the parsed data available in the req.body. useful for handling form submissions and other URL-encoded data without having to manually parse it.
+
+
+//will start a server and begin listening for incoming connections on the specified port
+app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`)
+});
